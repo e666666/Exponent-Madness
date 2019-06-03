650eb354b74d10429ae902a7a30d0b133702b934
Nyan cat <a0939712328@gmail.com>
2019-06-04 00:48:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -469,7 +469,7 @@ function format(a,placesUnder1000=3) { // formats numbers for display
 		m = 1;
 		e++;
 	}
-	if(a<1000) return sprintf(`%.${placesUnder1000}f`, a) // show up to 3 places by default
+	if(a<1000) return a.toFixed(placesUnder1000).replace(/\.?0*$/,'') // show up to 3 places by default
 	if (game.notation==2) return m+"e"+e; // scientific notation
 	if (game.notation==4) return "e"+(Math.round(1000*Math.log10(a))/1000); // log notation
 	var e2 = 3*Math.floor(e/3); // exponent for engineering notation
